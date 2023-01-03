a2976e97c19b200744753126c22fb7a3a93c4cb9
Firdavs Gadoev <54993849+decode1web@users.noreply.github.com>
2023-01-03 19:17:04 +0300

    deploy

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { useRoutes } from './routes'
+
+function App() {
+  const routes = useRoutes()
+  return (
+    <Router>
+      {routes}
+    </Router>
+  )
+}
+
+export default App
